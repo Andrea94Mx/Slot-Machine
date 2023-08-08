985ad6b81cc9564e04734f4adf85011f1861278d
Andrea Enriquez <fredie.info@gmail.com>
2023-08-08 00:16:06 -0600

    1.0

---
+++ project.js
@@ -0,0 +1,25 @@
+//1-Deposit money
+//2-Determine number of lines to bet on
+//3-collect bet amount
+//4-spin slot machine
+//5-check if user won
+//6-give winnings if any
+//7-play again
+
+//function deposit() {
+//return 1
+//}
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => { 
+        const depositAmount = prompt("Insert Coins: ");
+        const numberDepositAmount = parseInt(depositAmount);
+        
+};
+
+deposit();
+
+//Model
+//View
+//Controller
