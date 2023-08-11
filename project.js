1bb783efa503a8b96d2bf50431bae250b4122a51
Andrea Enriquez <fredie.info@gmail.com>
2023-08-10 20:18:25 -0600

    V2.0

--- project.js
+++ project.js
@@ -13,12 +13,63 @@
 const prompt = require("prompt-sync")();
 
 const deposit = () => { 
+        while (true) {
         const depositAmount = prompt("Insert Coins: ");
         const numberDepositAmount = parseInt(depositAmount);
         
+        if (isNaN(numberDepositAmount)|| numberDepositAmount<=0) {
+                console.log("Invalid deposit amount, try again");
+        } else {
+                return numberDepositAmount;
+        }
+        }
 };
 
-deposit();
+const lines = () => {
+        while (true) {
+        const linesAmount = prompt ("Pick your lines, cowboy! ");
+        const numberOfLines = parseInt(linesAmount);
+        
+        if (isNaN(numberOfLines)|| numberOfLines<=0 || numberOfLines > 3) {
+                console.log("Invalid number of lines, try a number between 1-3");
+        } else {
+                return numberOfLines;
+        }}
+
+};
+
+const numberDepositAmount = deposit();
+
+console.log("$" + numberDepositAmount);
+
+const numberOfLines = lines();
+
+const linesAmount = numberOfLines
+
+
+if (linesAmount===1) {
+        console.log(linesAmount + " Line");
+
+} else {
+console.log(linesAmount + " Lines");
+}
+
+let balance = numberDepositAmount;
+const getBet = (balance, lines) => {
+        while (true) {
+        const betAmount = prompt ("Enter your bet per line: ");
+        const numberBet = parseInt(betAmount);
+
+        if (isNaN(numberBet)|| numberBet > balance || numberBet <= 0 || numberBet > (balance / numberOfLines)) {
+                console.log("Invalid bet buddy, try again");
+        } else {
+                return numberBet;
+        }}
+
+}
+const numberBet = getBet(balance, numberOfLines);
+console.log("u just bet $" + (numberBet * numberOfLines));
+
 
 //Model
 //View
