51a0c675ac1a9d5bd147e42d7235d076f8621c3b
Andrea Enriquez <fredie.info@gmail.com>
2023-08-11 23:17:53 -0600

    V2.1

--- project.js
+++ project.js
@@ -12,6 +12,24 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_HOWMANY = {
+        "A": 2,
+        "B": 4,
+        "C": 6,
+        "D": 8
+}
+
+const SYMBOL_MULTIPLIER = {
+        "A": 5,
+        "B": 4,
+        "C": 3,
+        "D": 2
+
+}
+
 const deposit = () => { 
         while (true) {
         const depositAmount = prompt("Insert Coins: ");
@@ -36,6 +54,29 @@ const lines = () => {
                 return numberOfLines;
         }}
 
+};
+
+const spin = () => {
+        const symbols = [];
+        for  (const [symbol, howmany] of Object.entries(SYMBOLS_HOWMANY)){
+                for (let i = 0; i < howmany; i++){
+                symbols.push(symbol);
+                
+        }
+}
+const reels = [];
+
+for (let i=0; i<COLS; i++) {
+        reels.push([]);
+        const reelSymbols = [...symbols];
+for (let j=0; j<ROWS; j++) {
+const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+        const selectedSymbol = reelSymbols[randomIndex];
+        reels[i].push(selectedSymbol);
+        reelSymbols.splice(randomIndex, 1);
+}
+}
+console.log(reels);
 };
 
 const numberDepositAmount = deposit();
@@ -67,8 +108,13 @@ const getBet = (balance, lines) => {
         }}
 
 }
+
+
 const numberBet = getBet(balance, numberOfLines);
 console.log("u just bet $" + (numberBet * numberOfLines));
+
+spin();
+
 
 
 //Model
